a643d336813692649a6101dbe272ce27fe35b4a4
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2022-09-24 16:46:59 -0300

    layout finalizado

--- Departer/src/app/modules/shared/components/nav-bar/nav-bar.component.ts
+++ Departer/src/app/modules/shared/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
+   
   }
 
 }
