d3cf39116242c8721db27f9f7fa54dc910448529
MateRamires <matheushenriqueramires@gmail.com>
2022-12-01 18:59:56 -0300

    Cadastro e Login com validações

--- Departer/src/app/modules/autentificacao/components/cadastro/cadastro.component.ts
+++ Departer/src/app/modules/autentificacao/components/cadastro/cadastro.component.ts
@@ -1,3 +1,6 @@
+import { ValidatorField } from './../../../../helpers/ValidatorField';
+import { AuthService } from './../../services/auth.service';
+import { FormGroup, FormControl, Validators, AbstractControlOptions } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  registerForm!: FormGroup;
+
+  hasError = false;
+  errorMessage = "";
+
+  get f(): any {
+    return this.registerForm.controls;
+  }
+
+  constructor(private authService: AuthService) { }
+
+
+
 
   ngOnInit(): void {
+    this.registerValidation();
+  }
+
+  public registerValidation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmarSenha')
+    }
+
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      nome: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      senha: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmarSenha: new FormControl('', [Validators.required]),
+    }, formOptions);
+  }
+
+  public cssValidator(campoForm: FormControl): any {
+    return { 'is-invalid': campoForm.errors && campoForm.touched }
   }
 
 }
--- Departer/src/app/modules/autentificacao/components/login/login.component.ts
+++ Departer/src/app/modules/autentificacao/components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { AuthService } from './../../services/auth.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
@@ -11,32 +12,48 @@ export class LoginComponent implements OnInit {
 
   loginForm!: FormGroup;
 
-  constructor(private authService:AuthService) { }
-
-
   hasError = false;
   errorMessage = "";
 
+  get f(): any {
+    return this.loginForm.controls;
+  }
+
+  constructor(private authService:AuthService) { }
+
+
   ngOnInit(): void {
 
-   this.loginForm = new FormGroup({
-      email : new FormControl('', [Validators.required, Validators.email]),
-      password : new FormControl('', [Validators.required, Validators.minLength(8)]),
-    });
+    this.formValidation();
 
   }
 
-
-  login(){
-    this.authService.login({email: this.loginForm.controls['email'].value, password:this.loginForm.controls['password'].value}).subscribe({
-      next: (res) =>{
-        AuthService.setToken(res.data);
-      },
-      error: (err) => {
-        this.hasError = true;
-        this.errorMessage = err.error.message;
-      }
+  public formValidation(){
+    this.loginForm = new FormGroup({
+      email : new FormControl('', [Validators.required, Validators.email]),
+      senha : new FormControl('', [Validators.required, Validators.minLength(8)]),
     });
+  }
+
+
+  public login(): void {
+    if(this.loginForm.valid){
+      this.authService.login({email: this.loginForm.controls['email'].value, password:this.loginForm.controls['senha'].value}).subscribe({
+        next: (res) =>{
+          AuthService.setToken(res.data);
+        },
+        error: (err) => {
+          this.hasError = true;
+          this.errorMessage = err.error.message;
+        }
+      });
+    } else {
+
+    }
+  }
+
+  public cssValidator(campoForm: FormControl): any {
+    return { 'is-invalid': campoForm.errors && campoForm.touched }
   }
 
 }
