caaa64b4dc7c64c64c475d92b25a9de1a3e76625
MateRamires <matheushenriqueramires@gmail.com>
2022-12-09 02:28:52 -0300

    Instalando o fortAwesome Free e colocando icones

--- Departer/src/app/modules/administracao/administracao.module.ts
+++ Departer/src/app/modules/administracao/administracao.module.ts
@@ -1,14 +1,15 @@
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
-import { AdministracaoRoutingModule } from './administracao-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListaCategoriasComponent } from './components/lista-categorias/lista-categorias.component';
-import { CadastrarCategoriasComponent } from './components/cadastrar-categorias/cadastrar-categorias.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { AdministracaoComponent } from './components/administracao/administracao.component';
 
+import { AdministracaoRoutingModule } from './administracao-routing.module';
+
+import { ListaCategoriasComponent } from './components/lista-categorias/lista-categorias.component';
+import { CadastrarCategoriasComponent } from './components/cadastrar-categorias/cadastrar-categorias.component';
+import { AdministracaoComponent } from './components/administracao/administracao.component';
 
 
 @NgModule({
