1f2cdb7c9cf8d7d33cdd408b17ce1f729734bedf
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2023-01-08 17:01:56 -0300

    DatePicker agora em padrão nacional

--- Departer/src/app/modules/atividades/atividades.module.ts
+++ Departer/src/app/modules/atividades/atividades.module.ts
@@ -10,16 +10,18 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     ListaAtividadesComponent,
     NovaAtividadeComponent,
     EditarAtividadeComponent,
-    AtividadeComponent
+    AtividadeComponent,
   ],
   imports: [
     CommonModule,
+    FormsModule,
     AtividadesRoutingModule,
     MatDatepickerModule,
     MatFormFieldModule,
