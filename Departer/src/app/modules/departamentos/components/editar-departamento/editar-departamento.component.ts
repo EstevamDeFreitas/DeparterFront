7d5a30621883ca6df4a7656c7441ac83fe53562c
Francisco de Souza Rangel <franciscosouza20092000@gmail.com>
2023-03-22 22:50:45 -0300

    update

--- Departer/src/app/modules/departamentos/components/editar-departamento/editar-departamento.component.ts
+++ Departer/src/app/modules/departamentos/components/editar-departamento/editar-departamento.component.ts
@@ -80,7 +80,7 @@ export class EditarDepartamentoComponent implements OnInit {
       id: new FormControl('', [Validators.required]),
       nome: new FormControl('', [Validators.required]),
       descricao: new FormControl('', [Validators.required]),
-      imagem: new FormControl(''),
+      imagemUrl: new FormControl(''),
       maximoHorasDiarias: new FormControl('', [Validators.required]),
       maximoHorasMensais: new FormControl('', [Validators.required]),
     });
