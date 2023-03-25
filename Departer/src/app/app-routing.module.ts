6716c637b698bafaefefde23f2729172f2da2771
MateRamires <matheushenriqueramires@gmail.com>
2023-03-25 20:43:06 -0300

    Guardiao de rotas para usuarios nao logados feito

--- Departer/src/app/app-routing.module.ts
+++ Departer/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './modules/shared/guard/auth.guard';
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavBarComponent } from './modules/shared/components/nav-bar/nav-bar.component';
@@ -5,12 +6,12 @@ import { NavBarComponent } from './modules/shared/components/nav-bar/nav-bar.com
 const routes: Routes = [
     { path: "", pathMatch: "full", redirectTo: "autenticacao/login" },
     { path: '', loadChildren: () => import('./modules/autentificacao/autentificacao.module').then(m => m.AutentificacaoModule) },
-    { path: 'home', component: NavBarComponent, loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
-    { path: 'atividades', component: NavBarComponent, loadChildren: () => import('./modules/atividades/atividades.module').then(m => m.AtividadesModule) },
-    { path: 'administracao', component: NavBarComponent, loadChildren: () => import('./modules/administracao/administracao.module').then(m => m.AdministracaoModule) },
-    { path: 'configuracoes', component: NavBarComponent, loadChildren: () => import('./modules/configuracoes/configuracoes.module').then(m=>m.ConfiguracoesModule)},
-    { path: 'departamentos', component: NavBarComponent, loadChildren: () => import('./modules/departamentos/departamentos.module').then(m=>m.DepartamentosModule)},
-    { path: 'horas', component: NavBarComponent, loadChildren: () => import('./modules/horas/horas.module').then(m=>m.HorasModule)},
+    { path: 'home', component: NavBarComponent, loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard] },
+    { path: 'atividades', component: NavBarComponent, loadChildren: () => import('./modules/atividades/atividades.module').then(m => m.AtividadesModule), canActivate: [AuthGuard] },
+    { path: 'administracao', component: NavBarComponent, loadChildren: () => import('./modules/administracao/administracao.module').then(m => m.AdministracaoModule), canActivate: [AuthGuard] },
+    { path: 'configuracoes', component: NavBarComponent, loadChildren: () => import('./modules/configuracoes/configuracoes.module').then(m=>m.ConfiguracoesModule), canActivate: [AuthGuard]},
+    { path: 'departamentos', component: NavBarComponent, loadChildren: () => import('./modules/departamentos/departamentos.module').then(m=>m.DepartamentosModule), canActivate: [AuthGuard]},
+    { path: 'horas', component: NavBarComponent, loadChildren: () => import('./modules/horas/horas.module').then(m=>m.HorasModule), canActivate: [AuthGuard]},
     { path: '**', redirectTo: '/' }
 ];
 
---
+++ Departer/src/app/modules/shared/guard/auth.guard.ts
@@ -0,0 +1,25 @@
+import { AuthService } from './../../autentificacao/services/auth.service';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    const token = AuthService.getToken();
+    if (token) {
+      return true;
+    }
+
+    return this.router.parseUrl('/');
+  }
+
+}
