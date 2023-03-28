04b276f3411d3064727611d88bfab063718f9d6a
MateRamires <matheushenriqueramires@gmail.com>
2023-03-28 19:51:08 -0300

    "Ver mais" em cards do dashboard

--- Departer/src/app/modules/home/components/dashboard/dashboard.component.ts
+++ Departer/src/app/modules/home/components/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  public irParaAtividades(): void {
+    this.router.navigate(["/atividades/lista-atividades"]);
+  }
+
+  public irParaHoras(): void {
+    this.router.navigate(["/horas"]);
   }
 
 }
